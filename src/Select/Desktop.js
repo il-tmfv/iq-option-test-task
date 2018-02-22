61e984c28a4c923652b0969a1661ebc0a1868aaa
Ilya Timofeev <il.tmfv@gmail.com>
2018-02-23 00:18:55 +0300

    Add placeholder to Desktop

--- src/Select/Desktop.js
+++ src/Select/Desktop.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 export default class Desktop extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { focused: false };
+    this._onInputBlur = this._onInputBlur.bind(this);
+    this._onInputFocus = this._onInputFocus.bind(this);
+  }
+
   static propTypes = {
     dataSet: PropTypes.array.isRequired,
     value: PropTypes.any,
@@ -10,10 +18,31 @@ export default class Desktop extends Component {
     placeholder: PropTypes.string,
   };
 
+  _onInputBlur(e) {
+    this.setState({ focused: false });
+  }
+
+  _onInputFocus(e) {
+    this.setState({ focused: true });
+  }
+
   render() {
+    const { placeholder, id, value, onChange } = this.props;
+    const { focused } = this.state;
+
     return (
-      <div className="select">
-        <input type="text" />
+      <div className={classNames('select', { 'select_with-value': focused || value })}>
+        <label htmlFor={id} className="placeholder">
+          {placeholder}
+        </label>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          id={id}
+          onBlur={this._onInputBlur}
+          onFocus={this._onInputFocus}
+        />
       </div>
     );
   }
