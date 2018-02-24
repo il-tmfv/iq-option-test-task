01c3129e68c484be30274f26eb627fa783627a4b
Ilya Timofeev <il.tmfv@gmail.com>
2018-02-24 10:32:39 +0300

    Add second select for tests

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ import { onChange as onChangeAction } from './actions';
 import './App.styl';
 import { BOTTOM, TOP } from './consts';
 
+const dataSet = [
+  { value: '1', label: 'Арбуз' },
+  { value: '2', label: 'Груша' },
+  { value: '3', label: 'Дыня' },
+  { value: '4', label: 'Яблоко' },
+  { value: '5', label: 'Помидор' },
+  { value: '6', label: 'Слива' },
+  { value: '7', label: 'Картофель' },
+  { value: '8', label: 'Апельсин' },
+  { value: '9', label: 'Гранат' },
+  { value: '10', label: 'Вишня' },
+];
+
 const detectMobile = () => {
   return window.innerWidth <= 768;
 };
@@ -13,12 +26,19 @@ class App extends Component {
   constructor(props) {
     super(props);
     this._onChangeTop = this._onChangeTop.bind(this);
+    this._onChangeBottom = this._onChangeBottom.bind(this);
   }
 
   _onChangeTop(e) {
     const { dispatch } = this.props;
 
     dispatch(onChangeAction(TOP, typeof e === 'string' ? e : e.target.value));
+  }
+
+  _onChangeBottom(e) {
+    const { dispatch } = this.props;
+
+    dispatch(onChangeAction(BOTTOM, typeof e === 'string' ? e : e.target.value));
   }
 
   render() {
@@ -30,18 +50,15 @@ class App extends Component {
           value={this.props[TOP]}
           onChange={this._onChangeTop}
           placeholder="Выберите фрукт или овощ"
-          dataSet={[
-            { value: '1', label: 'Арбуз' },
-            { value: '2', label: 'Груша' },
-            { value: '3', label: 'Дыня' },
-            { value: '4', label: 'Яблоко' },
-            { value: '5', label: 'Помидор' },
-            { value: '6', label: 'Слива' },
-            { value: '7', label: 'Картофель' },
-            { value: '8', label: 'Апельсин' },
-            { value: '9', label: 'Гранат' },
-            { value: '10', label: 'Вишня' },
-          ]}
+          dataSet={dataSet}
+        />
+        <Select
+          id="select-component"
+          native={detectMobile()}
+          value={this.props[BOTTOM]}
+          onChange={this._onChangeBottom}
+          placeholder="Выберите фрукт или овощ"
+          dataSet={dataSet}
         />
       </div>
     );
